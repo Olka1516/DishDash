61665458ec7eb4ed203f7e7904bad832e61a4c6b
Olka1516 <olkakorolcuk@gmail.com>
2024-12-09 19:58:59 +0200

    DA-7: Created filters for receipts

--- pages/category.vue
+++ pages/category.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="container">
-    <div v-if="!data.length" class="category-empty">
-      <h1>Тут нема рецептів</h1>
-    </div>
-    <div v-else class="category">
-      <InputsFilter v-model="dataFilter" />
-      <InputsSearch v-model="dataSearch" />
-      <DetailedReceiptCard
-        v-for="item in data"
-        :title="item.title"
-        :description="item.description"
-        :id="item.id"
-        :image="item.image"
-      />
+    <div>
+      <div class="category">
+        <InputsFilter v-model="dataFilter" />
+        <InputsSearch v-model="dataSearch" />
+      </div>
+      <div v-if="!data.length" class="category-empty">
+        <h1>Тут нема рецептів</h1>
+      </div>
+      <div class="category" v-else>
+        <DetailedReceiptCard
+          v-for="item in data"
+          :title="item.title"
+          :description="item.description"
+          :id="item.id"
+          :image="item.image"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -28,12 +32,34 @@ const store = useReceiptsStore();
 const dataSearch = ref("");
 const dataFilter = ref(query.data ? [query.data] : []);
 
+const getFilteredReceipts = async () => {
+  const newData = await store.getReceiptsByFilters(
+    dataSearch.value,
+    dataFilter.value as string[]
+  );
+  data.value = newData;
+};
+
 onMounted(async () => {
   if (!store.receipts.length) {
     await store.getReceipts("receipts");
   }
-  data.value = store.receipts;
+  if (dataFilter.value.length) await getFilteredReceipts();
+  else data.value = store.receipts;
 });
+
+watch(
+  () => dataSearch.value,
+  async () => {
+    await getFilteredReceipts();
+  }
+);
+watch(
+  () => dataFilter.value.length,
+  async () => {
+    await getFilteredReceipts();
+  }
+);
 </script>
 
 <style scoped lang="scss">
